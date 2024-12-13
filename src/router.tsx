ef0b83134fcec5245dc4dd11e57da528052286f2
Joao Victor Ferreira Carvalho <137827716+JoaoVictorFerrer@users.noreply.github.com>
2024-12-13 11:27:07 +0100

    realizado la parte de autenticacion sin JWT

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,12 @@ import DashBoardView from "@/views/DashBoardView";
 import CreateProjectView from "./views/projects/CreateProjectView";
 import EditProjectView from "./views/projects/EditProjectView";
 import ProjectDetailsView from "./views/projects/ProjectDetailsView";
-
+import AutLayout from "./layouts/AutLayout";
+import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
+import ConfirmAccountView from "./views/auth/ConfirmAccountView";
+import RequestNewCodeView from "./views/auth/RequestNewCodeView";
+import ForgotPasswordView from "./views/auth/ForgotPasswordView";
 
 
 export default function Router() {
@@ -17,6 +22,13 @@ export default function Router() {
             <Route path="/projects/create" element={<CreateProjectView />} />
             <Route path="/projects/:projectId/edit" element={<EditProjectView/>} />
             <Route path="/projects/:projectId/" element={<ProjectDetailsView/>} />
+          </Route>
+          <Route element={<AutLayout/>}>
+            <Route path="auth/login" element={<LoginView/>}/>
+            <Route path="auth/register" element={<RegisterView/>}/>
+            <Route path="auth/confirm-account" element={<ConfirmAccountView/>}/>
+            <Route path="auth/request-code" element={<RequestNewCodeView/>}/>
+            <Route path="auth/forgot-password" element={<ForgotPasswordView/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/views/auth/ForgotPasswordView.tsx
@@ -0,0 +1,7 @@
+
+
+export default function ForgotPasswordView() {
+  return (
+    <div>ForgotPasswordView</div>
+  )
+}
